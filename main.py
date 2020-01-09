e304715764a4f1195a94f054bb4a3520efe177c8
Yashasvi Sriram <yash.3997@gmail.com>
2020-01-09 20:07:05 +0530

    moves logic handling from latex to python, by using python and mako templates 	moves all latex code to mako templates with .templatex extension 	uses python to generate tex files from templatex files 	moves latex packages to tex/ dir

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+
+from mako.template import Template
+
+TEMPLATEX_DIR = './templatex/'
+TEX_DIR = './tex/'
+MAIN_FILE_NAME = 'main.templatex'
+SUB_FILE_PATHS = []
+
+
+def change_extension_to_tex(original_path):
+    pre, ext = os.path.splitext(original_path)
+    return pre + '.tex'
+
+
+def add_sub_file(sub_file):
+    global SUB_FILE_PATHS
+    SUB_FILE_PATHS.append(sub_file)
+    return '\\subfile{%s}' % (change_extension_to_tex(sub_file))
+
+
+def save_as_tex(rendered, output_path):
+    os.makedirs(os.path.dirname(output_path), exist_ok=True)
+    output_file = open(output_path, 'w')
+    output_file.write(rendered)
+    output_file.close()
+
+
+main_template = Template(filename='{}{}'.format(TEMPLATEX_DIR, MAIN_FILE_NAME))
+output = main_template.render(add_sub_file=add_sub_file)
+save_as_tex(output, '{}{}'.format(TEX_DIR, change_extension_to_tex(MAIN_FILE_NAME)))
+
+for sub_file_path in SUB_FILE_PATHS:
+    template = Template(filename='{}{}'.format(TEMPLATEX_DIR, sub_file_path))
+    output = template.render()
+    save_as_tex(output, '{}{}'.format(TEX_DIR, change_extension_to_tex(sub_file_path)))
